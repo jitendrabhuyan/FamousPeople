cab91eea7b5e9d86892b0a160f7ee490370f926b
Jitendrabhuyan <bhuyan.jitendra@gmail.com>
2018-06-12 20:49:29 +0530

    adapter changes

--- app/src/main/java/app/demo/com/famouspeople/MainActivity.java
+++ app/src/main/java/app/demo/com/famouspeople/MainActivity.java
@@ -5,23 +5,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     FloatingActionButton fab;
+    ArrayList<String> users;
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        users = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            users.add("Jitendra #"+i);
+        }
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerViewAdapter = new UserAdapter(users);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
 
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
---
+++ app/src/main/java/app/demo/com/famouspeople/UserAdapter.java
@@ -0,0 +1,48 @@
+package app.demo.com.famouspeople;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+
+    ArrayList<String> users;
+
+    public UserAdapter(ArrayList<String> users) {
+        this.users = users;
+
+    }
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_row,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(UserAdapter.ViewHolder holder, int position) {
+        holder.firstName.setText(users.get(position));
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView firstName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            firstName = itemView.findViewById(R.id.first_name);
+        }
+    }
+
+
+}
